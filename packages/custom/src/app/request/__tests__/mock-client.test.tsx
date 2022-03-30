88311fc7fddf627312b574e1835f9f60ff0e42de
teobler <teobler@163.com>
2022-03-30 21:46:55 +0800

    add mock fetch case for testing request.
    Signed-off-by: teobler <teobler@163.com>

---
+++ packages/custom/src/app/request/__tests__/mock-client.test.tsx
@@ -0,0 +1,51 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import { CompWithRequest } from 'packages/custom/src/app/request/CompWithRequest';
+
+describe('mock-api-client', () => {
+  const originFetch = window.fetch;
+  const mockedFetch = jest.fn();
+
+  beforeAll(() => {
+    window.fetch = mockedFetch;
+  });
+
+  beforeEach(() => {
+    mockedFetch.mockResolvedValue({
+      json: () =>
+        Promise.resolve({
+          firstName: 'John',
+          lastName: 'Maverick',
+        }),
+    });
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  afterAll(() => {
+    window.fetch = originFetch;
+  });
+
+  it('should call mocked client when component send request', async () => {
+    render(<CompWithRequest />);
+
+    await waitFor(() => {
+      expect(
+        screen.getByText('firstName: John lastName: Maverick')
+      ).toBeInTheDocument();
+    });
+    expect(mockedFetch).toBeCalledWith('/test');
+  });
+
+  it('should call mocked client three times', async () => {
+    render(<CompWithRequest />);
+
+    await waitFor(() => {
+      expect(
+        screen.getByText('firstName: John lastName: Maverick')
+      ).toBeInTheDocument();
+    });
+    expect(mockedFetch).toBeCalledTimes(3);
+  });
+});
